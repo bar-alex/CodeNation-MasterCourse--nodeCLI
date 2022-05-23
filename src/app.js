7406de644d6af0bcf1666bfcacd9ec166b1f0e04
Eduard Alexandru <eduard.github@gmail.com>
2022-05-23 11:09:03 +0100

    some testing and added iquirer

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const {addMovie} = require("./utils")
+
+console.log( addMovie({title: "Spiderman", actor: "Andrew Garfield"}) );
+
+
+
+// accept at least movie title on command line
+
+
+
+// if user input has actor, capture this as well
+
+// add movie object to an array
+
+// console log the movie/movies to console
+
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+
+exports.addMovie = (movieObj) => {
+    // take a movieObj, add it to an array and the return the array
+    return [movieObj];
+};
+
+
+// module.exports = addMovie
